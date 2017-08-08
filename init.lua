e2750469133ed2d427e2f962f50b8b09545bdcce
AntumDeluge <antumdeluge@gmail.com>
2017-08-07 20:02:53 -0700

    Remove sheep mob

--- init.lua
+++ init.lua
@@ -11,7 +11,6 @@ mobs.intllib = S
 dofile(path .. "/chicken.lua") -- JKmurray
 dofile(path .. "/cow.lua") -- KrupnoPavel
 dofile(path .. "/rat.lua") -- PilzAdam
-dofile(path .. "/sheep.lua") -- PilzAdam
 dofile(path .. "/warthog.lua") -- KrupnoPavel
 dofile(path .. "/bee.lua") -- KrupnoPavel
 dofile(path .. "/bunny.lua") -- ExeterDad
